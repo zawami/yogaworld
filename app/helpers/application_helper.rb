080aa22805f0f75e11b338ffeed9330561e85c2c
zawami <61348118+zawami@users.noreply.github.com>
2020-02-23 03:25:40 +0000

    Provisional Top pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def document_title
+    if @title.present?
+      "#{@title} - YOGAWORLD"
+    else
+      "YOGAWORLD"
+    end
+  end
 end
