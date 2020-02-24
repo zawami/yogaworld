c189576e9398ec6ba1337652e565bf876a8981b1
zawami <61348118+zawami@users.noreply.github.com>
2020-02-24 12:18:38 +0000

    Error Page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
 
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddresRejected < ActionController::ActionControllerError;end
+ 
+ include ErrorHandlers if Rails.env.production?
+
   private def set_layout
     if params[:controller].match(%r{\A(instructor|organizer|customer)/})
       Regexp.last_match[1]
